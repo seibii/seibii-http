f5c57069c8c6ea70e26b7c079b9db2d8687dacbf
yuta sanada <sndyut0089@gmail.com>
2021-01-13 18:17:48 +0900

    :recycle: add json request option

--- lib/seibii/http/clients/json.rb
+++ lib/seibii/http/clients/json.rb
@@ -8,12 +8,13 @@ module Seibii
           @base = Base.new(logger: logger)
         end
 
-        def request(method:, uri:, params: nil, headers: {})
+        def request(method:, uri:, params: nil, headers: {}, need_verify_cert: false)
           response_body = @base.request(
             method: method,
             uri: uri,
             request_body: params&.yield_self { |p| Oj.dump(p, mode: :compat) },
-            headers: headers.merge('Accept': 'application/json', 'Content-Type': 'application/json')
+            headers: headers.merge('Accept': 'application/json', 'Content-Type': 'application/json'),
+            need_verify_cert: need_verify_cert
           )
           Oj.load(response_body, mode: :compat, symbol_keys: true)
         end
